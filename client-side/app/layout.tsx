7e1263a72b8ab77b31da8318c70def3b9e80c008
Afan-Saraya <afan@sarayasolutions.com>
2025-11-11 15:53:11 +0100

    initial commit

--- client-side/app/layout.tsx
+++ client-side/app/layout.tsx
@@ -1,7 +1,6 @@
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import "leaflet/dist/leaflet.css";
 
 export const metadata = {
   title: "Explore Sarajevo",
